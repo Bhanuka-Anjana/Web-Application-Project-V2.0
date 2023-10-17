5d157063d063533bb141c26bb2fc731aab1eb5bb
Bhanuka Anjana <85387483+Bhanu-2500@users.noreply.github.com>
2023-10-17 14:49:16 +0530

    add pagination

--- shop-frontend/src/components/products.jsx
+++ shop-frontend/src/components/products.jsx
@@ -1,20 +1,29 @@
 import React, { Component } from "react";
+import Pagination from "./common/pagination";
+import { paginate } from './../utils/paginate';
 
 class Products extends Component {
   state = {
-    products: []
+    products: [],
+    categories: [],
+    currentPage: 1,
+    pageSize: 4
   };
 
-  handleDelete = product => {
-    const products = this.state.products.filter(p => p._id !== product._id);
+  handleDelete = (product) => {
+    const products = this.state.products.filter((p) => p._id !== product._id);
     this.setState({ products });
   };
 
+  handlePageChange = page => {
+    this.setState({ currentPage: page });
+  };
+
   render() {
-    const { length } = this.state.products;
+    const { length , pageSize, currentPage , products: allProducts } = this.state.products;
 
     if (length === 0) return <p>There are no products in the database.</p>;
-
+const products = paginate(allProducts,currentPage,pageSize);
     return (
       <React.Fragment>
         <p>Showing {length} products in the database.</p>
@@ -29,7 +38,7 @@ class Products extends Component {
             </tr>
           </thead>
           <tbody>
-            {this.state.products.map(product => (
+            {products.map((product) => (
               <tr key={product._id}>
                 <td>{product.productName}</td>
                 <td>{product.category.name}</td>
@@ -47,6 +56,12 @@ class Products extends Component {
             ))}
           </tbody>
         </table>
+        <Pagination
+            itemsCount={length}
+            pageSize={pageSize}
+            currentPage={currentPage}
+            onPageChange={this.handlePageChange}
+          />
       </React.Fragment>
     );
   }
