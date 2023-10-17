a52c72fcb3e9e123aecc5bbc1c5239d3fe899579
Bhanuka Anjana <85387483+Bhanu-2500@users.noreply.github.com>
2023-10-17 17:20:19 +0530

    add routes

--- shop-frontend/src/App.js
+++ shop-frontend/src/App.js
@@ -1,20 +1,29 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { Route, Routes } from "react-router-dom";
 import LoginForm from "./components/loginForm";
+import NavBar from "./components/navBar";
 import RegisterForm from "./components/registrationForm";
-import ProductForm from "./components/productForm";
+import Products from "./components/products";
+import Orders from "./components/orders";
+import NotFound from "./components/notFound";
 
 class App extends Component {
-  render() { 
-    return (<React.Fragment>
-      <Routes>
-        <Route path="/" Component={ProductForm}/>
-        <Route path="/login" Component={LoginForm}/>
-        <Route path="/register" Component={RegisterForm}/>
-      </Routes>
-    </React.Fragment>);
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Routes>
+            <Route path="/register" element={<RegisterForm/>} />
+            <Route path="/login" element={<LoginForm/>} />
+            <Route path="/products" element={<Products/>} />
+            <Route path="/orders" element={<Orders/>} />
+            <Route path="/not-found" element={<NotFound/>} />
+          </Routes>
+        </main>
+      </React.Fragment>
+    );
   }
 }
- 
-export default App;
 
+export default App;
