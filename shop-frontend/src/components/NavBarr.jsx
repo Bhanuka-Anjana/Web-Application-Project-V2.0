2c7e1e2d8bd3226702970f0a4074386ad05fcb3e
Lahiru Madhuwantha <lahirumadhuwantha293@gmail.com>
2024-06-29 12:55:27 +0530

    Fix typo in Cafeteria label in NavBarr component

--- shop-frontend/src/components/NavBarr.jsx
+++ shop-frontend/src/components/NavBarr.jsx
@@ -27,7 +27,7 @@ export default function NavBarr() {
             navigate("/");
           }}
         >
-          feteria
+          defeteria
         </Navbar.Brand>
         <Nav>
           <Nav.Link
